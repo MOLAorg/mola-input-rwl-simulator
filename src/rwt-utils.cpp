3650b9b26ac9909b96d2062e2d7c8703ca105fcf
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2015-09-28 01:48:53 +0200

    copyright update

--- src/rwt-utils.cpp
+++ src/rwt-utils.cpp
@@ -1,7 +1,7 @@
 /* +---------------------------------------------------------------------------+
    |                       Recursive World Toolkit                             |
    |                                                                           |
-   |   Copyright (C) 2011  Jose Luis Blanco Claraco                            |
+   |   Copyright (C) 2011-2015  Jose Luis Blanco Claraco                       |
    |                                                                           |
    |      RWT is free software: you can redistribute it and/or modify          |
    |     it under the terms of the GNU General Public License as published by  |
